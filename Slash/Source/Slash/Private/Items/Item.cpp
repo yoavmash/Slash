fb0f8aac1d2c8859be69081b87bb22eeba62307a
yoavmash <yoavmashraki@walla.com>
2023-07-08 21:53:21 +0300

    finish 44

--- Slash/Source/Slash/Private/Items/Item.cpp
+++ Slash/Source/Slash/Private/Items/Item.cpp
@@ -15,33 +15,17 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString(TEXT("Item OnScreen Message!")));
-	}
 
 	TObjectPtr<UWorld> World = GetWorld();
-
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	
 	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"), DeltaTime);
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
 }
 
