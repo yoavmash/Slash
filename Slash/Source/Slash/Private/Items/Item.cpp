b7bd91c91f888b07daeb598c5c26011ef48c904e
yoav mashraki <yoavmashraki@walla.com>
2023-07-08 19:26:01 +0300

    finish 42

--- Slash/Source/Slash/Private/Items/Item.cpp
+++ Slash/Source/Slash/Private/Items/Item.cpp
@@ -3,22 +3,24 @@
 
 #include "Items/Item.h"
 
-// Sets default values
 AItem::AItem()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString(TEXT("Item OnScreen Message!")));
+	}
 }
 
-// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
