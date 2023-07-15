6501db87fc325aeb342c2366374974e6d0ffe14d
yoav mashraki <yoavmashraki@walla.com>
2023-07-15 16:34:53 +0300

    finish 84

---
+++ Slash/Source/Slash/Public/Characters/SlashAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+class ASlashCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class SLASH_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ASlashCharacter> SlashCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	TObjectPtr<UCharacterMovementComponent> SlashCharacterMovement;
+	
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+};
