c7633d0973e331adac373e14cb0d8ad2055541a8
yoav mashraki <yoavmashraki@walla.com>
2023-07-15 22:56:41 +0300

    finish 94

--- Slash/Source/Slash/Public/Items/Item.h
+++ Slash/Source/Slash/Public/Items/Item.h
@@ -38,6 +38,9 @@ protected:
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UFUNCTION()
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime = 0.25f;
