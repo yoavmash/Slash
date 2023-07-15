87f141bcb56f4bd024637d31f5933ac9377a9784
yoav mashraki <yoavmashraki@walla.com>
2023-07-15 22:38:05 +0300

    finish 92

--- Slash/Source/Slash/Public/Items/Item.h
+++ Slash/Source/Slash/Public/Items/Item.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class SLASH_API AItem : public AActor
 {
@@ -32,12 +34,19 @@ protected:
 
 	template<typename T>
 	T Avg(T First, T Second);
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime = 0.25f;
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UStaticMeshComponent> ItemMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
 };
 
 template<typename T>
